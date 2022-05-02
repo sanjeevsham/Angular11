06fd0b36c1704d6dbbb8990b70d2ef7139301fa7
sanjeevsham <sanjeev.neelakantan@chainsys.com>
2022-05-02 09:54:24 +0530

    empform

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmpformComponent } from './empform/empform.component';
+import { RouterModule,Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component'
+const routes: Routes=[
+    {path:'',component:HomeComponent},
+    {path:'empform',component:EmpformComponent},
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpformComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, ReactiveFormsModule ,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
